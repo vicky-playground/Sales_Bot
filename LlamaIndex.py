fdea95a0e7b47d7c9adb1acb91a6f95bcdd95bc1
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-08-22 14:52:03 -0400

    Update LlamaIndex.py

--- LlamaIndex.py
+++ LlamaIndex.py
@@ -1,24 +1,14 @@
-"""
-Before you can use LlamaIndex, you’ll need to access an LLM. By default, LlamaIndex uses GPT. You can get an OpenAI API key from their website. In the example code, I load my OpenAI API key from a .env file. However, you may directly input your key for your example locally if you’d like. Remove your key from the code before uploading it anywhere though!
-"""
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
-import dotenv
+from llama_index import SimpleDirectoryReader
+# load the document
+documents = SimpleDirectoryReader(input_files=["catalog.txt"]).load_data()
+
+from llama_index import GPTVectorStoreIndex
 import openai
 
-config = dotenv.dotenv_values(".env")
-openai.api_key = config['OPENAI_API_KEY']
+# use `OpenAI` to create the embedding for each node and store it in a Vector Store.
+openai.api_key = 'OPENAI_API_KEY'
 
-documents = SimpleDirectoryReader(input_files=["./data/catalog.txt"]).load_data()
-index = GPTVectorStoreIndex.from_documents(documents) # create the index 
+# build index
+index = GPTVectorStoreIndex.from_documents(documents)
 
-query_engine = index.as_query_engine()
-response=query_engine.query("What's the file about?")
-print(response)
-
-# Saving and Loading Your Index
-##  Saving your index saves on GPT tokens and lowers your LLM cost. Saving your index is remarkably easy. Call .storage_context.persist() on your index
-index.storage_context.persist() # This creates a folder called storage that contains three files: docstore.json, index_store.json, and vector_store.json. These files contain the documents, the index metadata, and the vector embeddings respectively. 
-
-"""
-The point of saving your index is so you can load it later. To load an index, we need two more imports from llama_index. We need StorageContext and load_index_from_storage. To rebuild the storage context, we pass the persistent storage directory to the StorageContext class. Once we’ve loaded the storage context, we call the load_index_from_storage function on it to reload the index.
-"""
+index.storage_context.persist() 
