88eecade22983866371c4714c03ec1d746d6f18e
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-08-22 14:51:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-import os
-from langchain import LLMChain
-from llama_index import StorageContext, load_index_from_storage
-import gradio as gr
 import openai
+openai.api_key = 'OPENAI_API_KEY'
 
-# Set up OpenAI API key
-openai.api_key = os.environ["OPENAI_API_KEY"]
+from llama_index import StorageContext, load_index_from_storage
 
-#Storing the conversation history in a List
+# Store the conversation history in a List
 conversation_history = []
 
-PROMPT_QUESTION = """
-Your name is IBM Help Assistant. You work for IBM. You are dedicated to every client's success.
-You are an expert in IBM products and helping a client to find the product they need.
-Your conversation with the human is recorded in the chat history below.
-
-History:
-"{history}"
-
-Now continue the conversation with the human. If you do not know the answer, say "I don't know" in a polite way.
-Human: {input}
-Assistant:"""
-
 def ask_bot(input_text):
+
     # rebuild storage context
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
     # load index
     index = load_index_from_storage(storage_context)
 
+    PROMPT_QUESTION = """
+    
+        Your name is IBM Help Assistant. You work for IBM. You are dedicated to every client's success.
+        You are an expert in IBM products and helping a client to find the product they need.
+        Your conversation with the human is recorded in the chat history below.
+
+        History:
+        "{history}"
+
+        Now continue the conversation with the human. If you do not know the answer based on the chat history and the new input from the client, politely admit it and therefore you need more information.
+        Human: {input}
+        Assistant:"""
+
+    # update conversation history
+    global conversation_history
     history_string = "\n".join(conversation_history)
-
     print(f"history_string: {history_string}")
-
+    
+    # query LlamaIndex and GPT-3.5 for the AI's response
     output = index.as_query_engine().query(PROMPT_QUESTION.format(history=history_string, input=input_text))
     print(f"output: {output}")
-    print(f"conversation_history: {conversation_history}")
-
+    
+    # update conversation history with user input and AI's response
     conversation_history.append(input_text)
     conversation_history.append(output.response)
+    
 
     return output.response
+
+import gradio as gr 
 
 with gr.Blocks() as demo:
     gr.Markdown('# IBM Help Assistant')
@@ -47,12 +50,16 @@ with gr.Blocks() as demo:
     gr.Markdown('### Sample messages:')
     gr.Markdown('#### :) I want to deploy an app for free')
     gr.Markdown('#### :) any advice on analytics?')
+    gr.Markdown('#### :) who are you?')
     gr.Markdown('#### :) what products do you have?')
-    gr.Markdown('#### :) I want to contact a real person')
+    gr.Markdown('#### :) what\'s my first request?')
     gr.Markdown('#### many more......')
-
+    
+    # create an input textbox and a submit button
     inputs=gr.inputs.Textbox(lines=4, label="Input Box", placeholder="Enter your text here")
     submit_btn = gr.Button("Submit") 
-    submit_btn.click(fn=ask_bot, outputs=gr.Textbox(lines=4, label="Output Box") )
+    # define the behavior of the submit button using the ask_bot function
+    submit_btn.click(fn=ask_bot, inputs=[inputs], outputs=gr.Textbox(lines=4, label="Output Box") )
 
+# launch the Gradio interface
 demo.launch()
